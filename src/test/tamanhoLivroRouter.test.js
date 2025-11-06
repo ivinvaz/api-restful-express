b37a3e83fbd5a8c8280b5e8088b7465844fa4975
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-11-06 16:34:01 -0300

    Adicionando testes de tamanholivrorouter

---
+++ src/test/tamanhoLivroRouter.test.js
@@ -0,0 +1,114 @@
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+
+const url = '/api/v1/livro/tamanho';
+
+let id = null;
+
+describe('Testes das rotas de tamanho', ()=>{
+    
+
+    test('POST:422 nome inexistente', async ()=>{
+        const resposta = await request.post(url).send({
+            "nome":null
+        })
+        expect(resposta.status).toBe(422);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Nome do tamanho é obrigatório");
+    });
+
+    test('POST:201', async ()=>{
+        const resposta = await request.post(url).send({
+            "nome":"Livro de Bolso" 
+        })
+        expect(resposta.status).toBe(201);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body._id).toBeDefined();
+        expect(resposta.body.nome).toBe("Livro de Bolso");
+        id = resposta.body._id;
+    });
+
+    test('GET:200', async()=>{
+        const resposta = await request.get(url);
+        expect(resposta.status).toBe(200);
+        expect(Array.isArray(resposta.body)).toBe(true);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+    });
+
+    test('GET:id:400 id inválido', async()=>{
+        const resposta = await request.get(`${url}/0`);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Parâmetro inválido");
+    });
+    
+    test('GET:id:404 id não encontrado', async()=>{
+        const resposta = await request.get(`${url}/000000000000000000000000`);
+        expect(resposta.status).toBe(404);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Tamanho não encontrado");
+    });
+
+    test('GET:id:200', async ()=>{
+        const resposta = await request.get(`${url}/${id}`);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.nome).toBe("Livro de Bolso");
+    });
+
+    test('PUT:id:400 id inválido', async()=>{
+        const resposta = await request.put(`${url}/0`).send({
+            "nome":"Livro de Bolso"
+        });
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Parâmetro inválido");
+    });
+    
+    test('PUT:id:404 id não encontrado', async()=>{
+        const resposta = await request.put(`${url}/000000000000000000000000`).send({
+            "nome":"Livro de Bolso"
+        });
+        expect(resposta.status).toBe(404);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Tamanho não encontrado");
+    });
+
+    test('PUT:id:200', async()=>{
+        const resposta = await request.put(`${url}/${id}`).send({
+            "nome":"Livro de Bolso" 
+        });
+        expect(resposta.status).toBe(200);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.nome).toBe("Livro de Bolso");
+    });
+
+    test('GET:id:200', async ()=>{
+        const resposta = await request.get(`${url}/${id}`);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.nome).toBe("Livro de Bolso");
+    });
+
+    test('DELETE:id:400 id inválido', async()=>{
+        const resposta = await request.delete(`${url}/0`)
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Parâmetro inválido");
+    });
+    
+    test('DELETE:id:404 id não encontrado', async()=>{
+        const resposta = await request.delete(`${url}/000000000000000000000000`)
+        expect(resposta.status).toBe(404);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Tamanho não encontrado");
+    });
+
+    test('DELETE:id:200', async()=>{
+        const resposta = await request.delete(`${url}/${id}`)
+        expect(resposta.status).toBe(204);
+    });
+
+    test('GET:id:404 id não encontrado', async()=>{
+        const resposta = await request.get(`${url}/${id}`);
+        expect(resposta.status).toBe(404);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Tamanho não encontrado");
+    });
+});
