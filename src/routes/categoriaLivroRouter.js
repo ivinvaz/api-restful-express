f77eccc521574add4bb4003c9f414d7c7da0f399
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-11-06 14:54:57 -0300

    Adicionando categoriaLivroRouter

--- src/routes/categoriaLivroRouter.js
+++ src/routes/categoriaLivroRouter.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const categoriaLivroController = require('../controller/categoriaLivroController');
 const router = express.Router();
+const autentificacao = require('../middleware/autentificacaoMiddleware');
 
-// POST - cria uma categoria >necessário autentificação<
-router.post('/', categoriaLivroController.adicionarCategoria);
+router.post('/', autentificacao.verificarTokenDeAutentificacao, categoriaLivroController.adicionarCategoria);
 
-// PUT/:id - edita uma categoria >necessário autentificação<
-router.put('/:id', categoriaLivroController.buscarCategoria, categoriaLivroController.editarCategoria);
+router.put('/:id', autentificacao.verificarTokenDeAutentificacao, categoriaLivroController.buscarCategoria, categoriaLivroController.editarCategoria);
 
-// GET - recebe todos as categorias  
 router.get('/', categoriaLivroController.listarCategorias);
 
-// GET/:id - recebe os dados da categoria e seus livros registrados  
 router.get('/:id', categoriaLivroController.buscarCategoria, categoriaLivroController.exibirCategoria);
 
-// DELETE - deleta a categoria, mas primeiro retira o seu registro dos livros >necessário autentificação<
-router.delete('/:id', categoriaLivroController.buscarCategoria, categoriaLivroController.deletarCategoria);
+router.delete('/:id', autentificacao.verificarTokenDeAutentificacao, categoriaLivroController.buscarCategoria, categoriaLivroController.deletarCategoria);
