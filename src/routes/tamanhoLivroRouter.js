25843c36856d8b8e6d232eb1ffdf5bec89f21919
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-11-06 14:56:11 -0300

    Corrigindo TamanhoLivroRouter

--- src/routes/tamanhoLivroRouter.js
+++ src/routes/tamanhoLivroRouter.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const tamanhoLivroController = require('../controller/tamanhoLivroController');
 const router = express.Router();
+const autentificacao = require('../middleware/autentificacaoMiddleware');
 
-// POST - cria um tamanho >necessário autentificação<  
-router.post('/', tamanhoLivroController.adicionarTamanho);
+router.post('/', autentificacao.verificarTokenDeAutentificacao, tamanhoLivroController.adicionarTamanho);
 
-// PUT/:id - edita um tamanho >necessário autentificação<  
-router.put('/:id', tamanhoLivroController.buscarTamanho, tamanhoLivroController.editarTamanho);
+router.put('/:id', autentificacao.verificarTokenDeAutentificacao, tamanhoLivroController.buscarTamanho, tamanhoLivroController.editarTamanho);
 
-// GET - recebe todos os tamanhos 
 router.get('/', tamanhoLivroController.listarTamanhos);
 
-// GET/:id - recebe os dados do tamanho e seus livros registrados no estoque
 router.get('/:id', tamanhoLivroController.buscarTamanho, tamanhoLivroController.exibirTamanho);
 
-// DELETE - deleta a categoria, mas primeiro retira o seu registro do estoque e impede o delete caso tenham itens em estoque >necessário autentificação< 
-router.delete('/:id', tamanhoLivroController.buscarTamanho, tamanhoLivroController.deletarTamanho);
+router.delete('/:id', autentificacao.verificarTokenDeAutentificacao, tamanhoLivroController.buscarTamanho, tamanhoLivroController.deletarTamanho);
