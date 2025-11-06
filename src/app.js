72707a4a7cdee70b39fc87c5d285f314ee4e7e68
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-11-06 15:15:18 -0300

    Corrigindo link das rotas no app

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api-documentation',documentacaoSwaggerRouter);
+app.use('/api/v1/api-documentation',documentacaoSwaggerRouter);
 
-app.use('/usuario',usuarioRouter);
+app.use('/api/v1/usuario',usuarioRouter);
 
-app.use('/livro',livroRouter);
-app.use('/livro/categoria',categoriaLivroRouter);
-app.use('/livro/tamanho',tamanhoLivroRouter);
-app.use('/livro/estoque',estoqueLivroRouter);
-app.use('/livro/autor',autorLivroRouter);
+app.use('/api/v1/livro',livroRouter);
+app.use('/api/v1/livro/categoria',categoriaLivroRouter);
+app.use('/api/v1/livro/tamanho',tamanhoLivroRouter);
+app.use('/api/v1/livro/estoque',estoqueLivroRouter);
+app.use('/api/v1/livro/autor',autorLivroRouter);
 
 module.exports = app;
