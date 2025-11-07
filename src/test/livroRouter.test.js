d5e3f51e797741fc42d66a5b6d06a5f3a9048cda
Ivin Vaz Braga Santos <133714762+ivinvaz@users.noreply.github.com>
2025-11-07 16:17:45 -0300

    Corrigindo e implementando melhor o teste de livrorouter

--- src/test/livroRouter.test.js
+++ src/test/livroRouter.test.js
@@ -7,40 +7,55 @@ const urlAutor = '/api/v1/livro/autor';
 const urlCategoria = '/api/v1/livro/categoria';
 
 let id = null;
+let token = null;
 
+const tempo = Date.now();
+const numeroAleatorio = Math.floor(Math.random() * 1000);
+let nomeUser = `nomeUserTest${tempo}`;
+let emailUser = `emailUserTest${tempo}@gmail.com`;
+let senhaUser = `senhaUserTest${tempo}`;
+let nome = `nomeTest${tempo}`;
+let paginas = numeroAleatorio;
+let autor = `autorTest${tempo}`;
+let categoria = `categoriaTest${tempo}`;
 
 describe('Testes das rotas de livro', ()=>{
     
-    test('MOCK Autor', async ()=>{
-        const resposta = await request.post(urlAutor).send({
-            "nome":"Axe Of Assis",
-            "idade":24,
+    beforeAll(async () => {
+        await request.post("/api/v1/usuario").send({
+            "nome":nomeUser,
+            "email":emailUser,
+            "senha":senhaUser
+        });
+
+        const mockLogin = await request.post("/api/v1/usuario/login").send({
+            "email":emailUser,
+            "senha":senhaUser
+        });
+        
+        expect(mockLogin.status).toBe(200);
+        expect(mockLogin.body.token).toBeDefined();
+        token = mockLogin.body.token;
+
+        const mockAutor = await request.post(urlAutor).set("authorization",`Bearer ${token}`).send({
+            "nome":autor,
+            "idade":paginas,
             "nacionalidade":"Brasileiro"  
         })
-        expect(resposta.status).toBe(201);
-        expect(resposta.headers['content-type']).toMatch(/json/);
-        expect(resposta.body._id).toBeDefined();
-        expect(resposta.body.nome).toBe("Axe Of Assis");
-        expect(resposta.body.idade).toBe(24);
-        expect(resposta.body.nacionalidade).toBe("Brasileiro");
-    });
+        expect(mockAutor.status).toBe(201);
 
-    test('MOCK Categoria', async ()=>{
-        const resposta = await request.post(urlCategoria).send({
-            "nome":"Literatura" 
+        const mockCategoria = await request.post(urlCategoria).set("authorization",`Bearer ${token}`).send({
+            "nome":categoria
         })
-        expect(resposta.status).toBe(201);
-        expect(resposta.headers['content-type']).toMatch(/json/);
-        expect(resposta.body._id).toBeDefined();
-        expect(resposta.body.nome).toBe("Literatura");
+        expect(mockCategoria.status).toBe(201);
     });
 
     test('POST:422 nome inexistente', async ()=>{
-        const resposta = await request.post(url).send({
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
             "nome":null,
-            "paginas":10,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+            "paginas":paginas,
+            "autor":autor,
+            "categoria":categoria 
         })
         expect(resposta.status).toBe(422);
         expect(resposta.headers['content-type']).toMatch(/json/);
@@ -48,11 +63,11 @@ describe('Testes das rotas de livro', ()=>{
     });
 
     test('POST:422 paginas inexistente', async ()=>{
-        const resposta = await request.post(url).send({
-            "nome":"Memórias do Brás",
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
             "paginas":null,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+            "autor":autor,
+            "categoria":categoria 
         })
         expect(resposta.status).toBe(422);
         expect(resposta.headers['content-type']).toMatch(/json/);
@@ -60,11 +75,11 @@ describe('Testes das rotas de livro', ()=>{
     });
 
     test('POST:422 paginas incorreta', async ()=>{
-        const resposta = await request.post(url).send({
-            "nome":"Memorias do Brás",
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
             "paginas":"abc",
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+            "autor":autor,
+            "categoria":categoria 
         })
         expect(resposta.status).toBe(422);
         expect(resposta.headers['content-type']).toMatch(/json/);
@@ -72,11 +87,11 @@ describe('Testes das rotas de livro', ()=>{
     });
 
     test('POST:404 autor inexistente', async ()=>{
-        const resposta = await request.post(url).send({
-            "nome":"Memorias do Brás",
-            "paginas":"abc",
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas,
             "autor":"Joao Pessoa",
-            "categoria":"Literatura"  
+            "categoria":categoria 
         })
         expect(resposta.status).toBe(404);
         expect(resposta.headers['content-type']).toMatch(/json/);
@@ -84,31 +99,43 @@ describe('Testes das rotas de livro', ()=>{
     });
 
     test('POST:404 categoria inexistente', async ()=>{
-        const resposta = await request.post(url).send({
-            "nome":"Memorias do Brás",
-            "paginas":"abc",
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas,
             "autor":"Joao Pessoa",
-            "categoria":"Literatura"  
+            "categoria":"Outra categoria" 
         })
         expect(resposta.status).toBe(404);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Categoria não encontrada");
     });
 
-    test('POST:201', async ()=>{
+    test('POST:401 token ausente', async ()=>{
         const resposta = await request.post(url).send({
-            "nome":"Memorias do Brás",
-            "paginas":24,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+            "nome":nome,
+            "paginas":paginas,
+            "autor":autor,
+            "categoria":categoria 
+        })
+        expect(resposta.status).toBe(401);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Token ausente");
+    });
+
+    test('POST:201', async ()=>{
+        const resposta = await request.post(url).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas,
+            "autor":autor,
+            "categoria":categoria 
         })
         expect(resposta.status).toBe(201);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body._id).toBeDefined();
-        expect(resposta.body.nome).toBe("Memorias do Brás");
-        expect(resposta.body.paginas).toBe(24);
-        expect(resposta.body.autor.nome).toBe("Axe of Assis");
-        expect(resposta.body.categoria.nome).toBe("Literatura");
+        expect(resposta.body.nome).toBe(nome);
+        expect(resposta.body.paginas).toBe(paginas);
+        expect(resposta.body.autor.nome).toBe(autor);
+        expect(resposta.body.categoria.nome).toBe(categoria);
         id = resposta.body._id;
     });
 
@@ -121,6 +148,7 @@ describe('Testes das rotas de livro', ()=>{
 
     test('GET:id:400 id inválido', async()=>{
         const resposta = await request.get(`${url}/0`);
+        expect(resposta.status).toBe(400);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Parâmetro inválido");
     });
@@ -134,75 +162,98 @@ describe('Testes das rotas de livro', ()=>{
 
     test('GET:id:200', async ()=>{
         const resposta = await request.get(`${url}/${id}`);
+        expect(resposta.status).toBe(200);
         expect(resposta.headers['content-type']).toMatch(/json/);
-        expect(resposta.body.nome).toBe("Memorias do Brás");
-        expect(resposta.body.paginas).toBe(24);
-        expect(resposta.body.autor.nome).toBe("Axe of Assis");
+        expect(resposta.body.nome).toBe(nome);
+        expect(resposta.body.paginas).toBe(paginas);
+        expect(resposta.body.autor.nome).toBe(autor);
         expect(resposta.body.categoria.nome).toBe("Literatura");
     });
 
     test('PUT:id:400 id inválido', async()=>{
-        const resposta = await request.put(`${url}/0`).send({
-            "nome":"Memorias do Brás",
-            "paginas":34,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+        const resposta = await request.put(`${url}/0`).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas+1,
+            "autor":autor,
+            "categoria":categoria 
         });
+        expect(resposta.status).toBe(400);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Parâmetro inválido");
     });
     
     test('PUT:id:404 id não encontrado', async()=>{
-        const resposta = await request.put(`${url}/000000000000000000000000`).send({
-            "nome":"Memorias do Brás",
-            "paginas":34,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+        const resposta = await request.put(`${url}/000000000000000000000000`).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas+1,
+            "autor":autor,
+            "categoria":categoria 
         });
         expect(resposta.status).toBe(404);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Livro não encontrado");
     });
 
-    test('PUT:id:200', async()=>{
+    test('PUT:id:401 token ausente', async()=>{
         const resposta = await request.put(`${url}/${id}`).send({
-            "nome":"Memorias do Brás",
-            "paginas":34,
-            "autor":"Axe of Assis",
-            "categoria":"Literatura"  
+            "nome":nome,
+            "paginas":paginas+1,
+            "autor":autor,
+            "categoria":categoria 
+        });
+        expect(resposta.status).toBe(401);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Token ausente");
+    });
+
+    test('PUT:id:200', async()=>{
+        const resposta = await request.put(`${url}/${id}`).set("authorization",`Bearer ${token}`).send({
+            "nome":nome,
+            "paginas":paginas+1,
+            "autor":autor,
+            "categoria":categoria 
         });
         expect(resposta.status).toBe(200);
         expect(resposta.headers['content-type']).toMatch(/json/);
-        expect(resposta.body.nome).toBe("Memorias do Brás");
-        expect(resposta.body.paginas).toBe(34);
-        expect(resposta.body.autor.nome).toBe("Axe of Assis");
+        expect(resposta.body.nome).toBe(nome);
+        expect(resposta.body.paginas).toBe(paginas+1);
+        expect(resposta.body.autor.nome).toBe(autor);
         expect(resposta.body.categoria.nome).toBe("Literatura");
     });
 
     test('GET:id:200', async ()=>{
         const resposta = await request.get(`${url}/${id}`);
+        expect(resposta.status).toBe(200);
         expect(resposta.headers['content-type']).toMatch(/json/);
-        expect(resposta.body.nome).toBe("Memorias do Brás");
-        expect(resposta.body.paginas).toBe(34);
-        expect(resposta.body.autor.nome).toBe("Axe of Assis");
+        expect(resposta.body.nome).toBe(nome);
+        expect(resposta.body.paginas).toBe(paginas+1);
+        expect(resposta.body.autor.nome).toBe(autor);
         expect(resposta.body.categoria.nome).toBe("Literatura");
     });
 
     test('DELETE:id:400 id inválido', async()=>{
-        const resposta = await request.delete(`${url}/0`)
+        const resposta = await request.delete(`${url}/0`).set("authorization",`Bearer ${token}`);
+        expect(resposta.status).toBe(400);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Parâmetro inválido");
     });
     
     test('DELETE:id:404 id não encontrado', async()=>{
-        const resposta = await request.delete(`${url}/000000000000000000000000`)
+        const resposta = await request.delete(`${url}/000000000000000000000000`).set("authorization",`Bearer ${token}`);
         expect(resposta.status).toBe(404);
         expect(resposta.headers['content-type']).toMatch(/json/);
         expect(resposta.body.msg).toContain("Livro não encontrado");
     });
 
-    test('DELETE:id:200 id não encontrado', async()=>{
-        const resposta = await request.delete(`${url}/${id}`)
+    test('DELETE:id:401 token ausente', async()=>{
+        const resposta = await request.delete(`${url}/${id}`);
+        expect(resposta.status).toBe(401);
+        expect(resposta.headers['content-type']).toMatch(/json/);
+        expect(resposta.body.msg).toContain("Token ausente");
+    });
+
+    test('DELETE:id:200', async()=>{
+        const resposta = await request.delete(`${url}/${id}`).set("authorization",`Bearer ${token}`);
         expect(resposta.status).toBe(204);
     });
 
@@ -213,4 +264,3 @@ describe('Testes das rotas de livro', ()=>{
         expect(resposta.body.msg).toContain("Livro não encontrado");
     });
 });
-
